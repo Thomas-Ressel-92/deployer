cf163ab164efdca650007f7b20dbade011855cc6
rml <rml@NBDR326.salt-solutions.de>
2019-11-08 15:01:39 +0100

    NEW releases logfile and task 'deploy:cleanup_old_releases' added

--- Recipes/Deploy.php
+++ Recipes/Deploy.php
@@ -3,6 +3,7 @@
 namespace Deployer;
 
 use Deployer\Exception\Exception;
+use Deployer\Type\Csv;
 use function Deployer\Support\str_contains;
 
 desc('Preparing host for deploy');
@@ -33,6 +34,10 @@ task('deploy:prepare', function () {
     
     // Create shared dir.
     run("cd {{deploy_path}} && if [ ! -d shared ]; then mkdir shared; fi");
+    
+    // Create .dep dir and releases log file.
+    run("cd {{deploy_path}} && if [ ! -d .dep ]; then mkdir dep; fi");
+    run("cd {{deploy_path}} && if [ ! -f .dep/releases ]; then touch .dep/release; fi");
     
     //test if the release already exists on server, if not, create folder
     $result = test('[ -d {{deploy_path}}/{{release_path}} ] ');
@@ -90,7 +95,85 @@ task('deploy:create_shared_links', function() {
         run('cd {{deploy_path}}; export set CYGWIN=winsymlinks:nativestrict; ln -nfs --relative ' . $shared_dir . '/' . $dir . ' ' . get('release_path') . '/' . $dir);
     };
 });
+
+task('deploy:cleanup_old_releases', function() {
+    cd('{{deploy_path}}');
     
+    // If there is no releases return empty list.
+    if (!test('[ -d releases ] && [ "$(ls -A releases)" ]')) {
+        $releases = [];
+    }
+    
+    // Will list only dirs in releases.
+    $list = explode("\n", run('cd releases && ls -t -1 -d */'));
+    
+    // Prepare list.
+    $list = array_map(function ($release) {
+        return basename(rtrim(trim($release), '/'));
+    }, $list);
+        
+    $releases = []; // Releases list.
+    
+    // Collect releases based on .dep/releases info.
+    // Other will be ignored.
+    
+    if (test('[ -f .dep/releases ]')) {
+        $keepReleases = get('keep_releases');
+        if ($keepReleases === -1) {
+            $csv = run('cat .dep/releases');
+        } else {
+            // Instead of `tail -n` call here can be `cat` call,
+            // but on hosts with a lot of deploys (more 1k) it
+            // will output a really big list of previous releases.
+            // It spoils appearance of output log, to make it pretty,
+            // we limit it to `n*2 + 5` lines from end of file (15 lines).
+            // Always read as many lines as there are release directories.
+            $csv = run("tail -n " . max(count($releases), ($keepReleases * 2 + 5)) . " .dep/releases");
+        }
+        
+        $metainfo = Csv::parse($csv);
+        
+        for ($i = count($metainfo) - 1; $i >= 0; --$i) {
+            if (is_array($metainfo[$i]) && count($metainfo[$i]) >= 2) {
+                list(, $release) = $metainfo[$i];
+                $index = array_search($release, $list, true);
+                if ($index !== false) {
+                    $releases[] = $release;
+                    unset($list[$index]);
+                }
+            }
+        }
+    }
+    
+    // Metainfo.
+    $date = run('date +"%Y%m%d%H%M%S"');
+    
+    // Save metainfo about release
+    run("echo '{$date},{{release_name}}' >> .dep/releases");
+    
+    $releaseName = get('release_name');
+    
+    // Add to releases list
+    array_unshift($releases, $releaseName);
+    
+    $keep = get('keep_releases');
+    
+    if ($keep === -1) {
+        // Keep unlimited releases.
+        return;
+    }
+    
+    while ($keep > 0) {
+        array_shift($releases);
+        --$keep;
+    }
+    
+    foreach ($releases as $release) {
+        run("rm -rf {{deploy_path}}/releases/{$release}");
+    }
+    
+    
+});
 task('deploy:show_release_names', function () {
     writeln('Deployed to new release: {{deploy_path}}/{{release_path}}');
     /*if(has('previous_release')) {
