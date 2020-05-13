7cb8b62d33042403b0021a5fdbd171ce6ae3e0fa
Andrej Kabachnik <andrej_ka@gmx.de>
2020-05-13 11:22:48 +0200

    FIX composer.lock SQL migration

--- Install/Sql/MySQL/Migrations/1.0/20200513_1104_01_NEW_save_composer_lock.sql
+++ Install/Sql/MySQL/Migrations/1.0/20200513_1104_01_NEW_save_composer_lock.sql
@@ -1,9 +1,9 @@
 -- UP
 
 ALTER TABLE `axxdep_build`
-	ADD COLUMN `composer.lock` LONGTEXT NULL DEFAULT NULL AFTER `log`;
+	ADD COLUMN `composer_lock` LONGTEXT NULL DEFAULT NULL AFTER `log`;
 	
 -- DOWN
 	
 ALTER TABLE `axxdep_build`
-	DROP COLUMN `composer.lock`;
+	DROP COLUMN `composer_lock`;
